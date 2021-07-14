4e20d5e1b714812e03bc4b4f273fe156a8ab1fb4
Flavio <conectef@gmail.com>
2021-07-14 18:17:01 -0300

    1

---
+++ main.cs
@@ -0,0 +1,50 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+using System; 
+
+class minhaClasse {
+
+    static void Main(string[] args) { 
+
+            int ddd = Convert.ToInt32(Console.ReadLine());
+
+            switch (ddd)
+            {
+                case 61:
+                    Console.WriteLine("Brasilia");
+                    break;
+                case 71:                             //complete os DDD's corretamente
+                    Console.WriteLine("Salvador");
+                    break;
+                case 11:
+                    Console.WriteLine("Sao Paulo");
+                    break;
+                case 21:
+                    Console.WriteLine("Rio de Janeiro");
+                    break;
+                case 32:
+                    Console.WriteLine("Juiz de Fora");
+                    break;
+                case 19:
+                    Console.WriteLine("Campinas");
+                    break;
+                case 27:
+                    Console.WriteLine("Vitoria");
+                    break;
+                case 31:
+                    Console.WriteLine("Belo Horizonte");
+                    break;
+                default:
+                    Console.WriteLine("DDD nao cadastrado");
+                    break;
+        }
+
+    }
+    
+}
